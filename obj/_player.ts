66691408ab53a8503febf8ad0d1dd7a139ee6152
Rihards Polis <rihards99@gmail.com>
2016-02-22 15:17:35 +0000

    added basic health bars

--- obj/_player.ts
+++ obj/_player.ts
@@ -2,6 +2,12 @@ module GameModule {
 	export class Player extends Phaser.Sprite {
 		walkSpeed: number = 150;
 		slowDownSpeed: number = 15; // Needs to be a fraction of the walk speed.
+		
+		bar: any;
+		health: number = 100;
+		maxHealth: number = 100;
+		barHeight: number = 5;
+		barWidth: number = 40;
 
 		constructor(game: GameModule.Game, x: number, y: number, sprite: string) {
 			super(game, x, y, sprite, 0);
@@ -9,6 +15,8 @@ module GameModule {
 			game.add.existing(this);
 			game.physics.enable(this, Phaser.Physics.ARCADE);
 			this.body.width = 25;
+			// the creation position of the bar isn't important, since it gets updated every cycle
+        	this.bar = game.add.sprite(0, 0, this.getHealthBar());
 		}
 
 		controls() {
@@ -33,6 +41,35 @@ module GameModule {
 				this.body.velocity.x = this.walkSpeed;
 				this.frame = 3;
 			}
+			
+			// TODO: TESTING
+			if (this.game.input.keyboard.isDown(Phaser.Keyboard.E)) {
+				this.health -= 1;
+			}
+			
+			if (this.game.input.keyboard.isDown(Phaser.Keyboard.Q)) {
+				this.health += 1;
+			}
+			
+			this.bar.x = this.x - (this.barWidth * 0.5);
+			this.bar.y = this.y - 30;
+			this.bar.loadTexture(this.getHealthBar());
+		}
+		
+		getHealthBar(): Phaser.BitmapData {
+			var bmd = this.game.make.bitmapData(this.barWidth, this.barHeight);
+			bmd.context.fillStyle = '#000';
+			bmd.context.fillRect(0, 0, this.barWidth, this.barHeight);
+			var percent = this.health / this.maxHealth
+			if (percent < 0.3)
+	        	bmd.context.fillStyle = '#f00'; // red
+	        else if (percent < 0.6)
+	        	bmd.context.fillStyle = '#ff0'; // yellow
+	        else 
+	        	bmd.context.fillStyle = '#0f0'; // green
+	        
+			bmd.context.fillRect(0, 0, percent * this.barWidth, this.barHeight);
+			return bmd
 		}
 
 		protected slowDown(coord) {
--- states/game/hostState.ts
+++ states/game/hostState.ts
@@ -2,8 +2,14 @@ module GameModule {
 	export class HostState extends State {
 		player: HostPlayer;
 		server: Server;
+		bar: any;
+		barProgress: number = 128;
 
 		create() {
+        	// this.bar = this.add.bitmapData(128, 8);
+        	// this.game.add.sprite(this.game.world.centerX - (this.bar.width * 0.5), this.game.world.centerY, this.bar);
+        	// this.game.add.tween(this).to({barProgress: 0}, 2000, null, true, 0, Infinity);
+			
 			this.server = new Server(this);
 			this.server.start();
 			super.create();
@@ -39,6 +45,15 @@ module GameModule {
 				super.update();
 				// Broadcast the current state
 				this.server.syncState(this.getPlayers());
+				
+				
+				// this.bar.context.clearRect(0, 0, this.bar.width, this.bar.height);
+        		// some simple colour changing to make it look like a health bar
+		        // this.bar.context.fillStyle = '#f00';
+		        // draw the bar
+		        // this.bar.context.fillRect(0, 0, this.barProgress, 8);
+		        // important - without this line, the context will never be updated on the GPU when using webGL
+		        // this.bar.dirty = true;
 			}
 		}
 	}
